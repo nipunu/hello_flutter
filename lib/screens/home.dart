9297552a03e666fa8aa4d73dccbdbb9c54adbb9b
Nipunu Wijesingha <nipunu.wijesingha@gmail.com>
2021-03-23 14:10:49 +0530

    added the sayHello function

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-          color: Colors.deepPurple,
-          child: Center(
-            child: Text(
-              "Hello Fluter",
-              textDirection: TextDirection.ltr,
-              style: TextStyle(color: Colors.white, fontSize: 36.0),
-            ),
-          ),
-        );
+      color: Colors.deepPurple,
+      child: Center(
+        child: Text(
+          sayHello(),
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.white, fontSize: 36.0),
+        ),
+      ),
+    );
   }
 
+  String sayHello() {
+    String hello;
+    DateTime now = new DateTime.now();
+    int hour = now.hour;
+    if (hour < 12) {
+      hello = "Good Morning";
+    } else if (hour < 18) {
+      hello = "Good Afternoon";
+    } else {
+      hello = "Good Evening";
+    }
+
+int minutes = now.minute;
+
+
+    return hello + " Time is " + hour.toString() + ":" + minutes.toString();
+  }
 }
